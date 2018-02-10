15541275b6a3f520fdd9c77f5a767a5a78475385
brian leeson <brianeleeson@gmail.com>
2018-02-10 13:03:31 -0800

    init

---
+++ Encrypt.py
@@ -0,0 +1,29 @@
+"""
+Author(s): Brian Leeson
+This file is responsible for encrypting and decrypting messages.
+
+"""
+
+
+def encrypt(plaintext, key):
+    """
+
+    :param plaintext:
+    :param key:
+    :return:
+    """
+    cyphertext = ""
+
+    return cyphertext
+
+
+def decrypt(cyphertext, key):
+    """
+
+    :param cyphertext:
+    :param key:
+    :return:
+    """
+    plaintext = ""
+
+    return plaintext
